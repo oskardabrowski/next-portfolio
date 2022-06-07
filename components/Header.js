c4e30a4c8de83ebc69c9eb82b847da486b41c58c
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-06-07 23:32:08 +0200

    Adding components folder to file tracking

---
+++ components/Header.js
@@ -0,0 +1,105 @@
+import styles from "../styles/StyleBundle";
+
+const { HomeHeaderStyle } = styles;
+
+export default function HomeHeader() {
+	return (
+		<>
+			<HomeHeaderStyle>
+				<div className="AbstractPicture">
+					<div className="AbstractPicture-layer3">
+						<div className="AbstractPicture-layer3-hello">
+							<h1>
+								<div>
+									<main>
+										Dzień dobry<span>!</span>
+									</main>
+								</div>
+								<div>
+									<main>Jestem Oskar Dąbrowski</main>
+								</div>
+								<div>
+									<main>
+										<span>Front-End</span> Developer
+									</main>
+								</div>
+							</h1>
+						</div>
+					</div>
+					<div className="AbstractPicture-layer2">
+						<div className="AbstractPicture-layer2-obj1"></div>
+						<div className="AbstractPicture-layer2-obj2"></div>
+						<div className="AbstractPicture-layer2-obj3">
+							<span></span>
+							<span></span>
+							<span>
+								<div></div>
+								<div></div>
+							</span>
+						</div>
+						<div className="AbstractPicture-layer2-obj4">
+							<span></span>
+						</div>
+						<div className="AbstractPicture-layer2-obj5"></div>
+						<div className="AbstractPicture-layer2-obj6"></div>
+						<div className="AbstractPicture-layer2-obj7"></div>
+						<div className="AbstractPicture-layer2-obj8"></div>
+						<div className="AbstractPicture-layer2-obj9"></div>
+						<div className="AbstractPicture-layer2-obj10"></div>
+						<div className="AbstractPicture-layer2-obj11">
+							<span></span>
+							<span></span>
+						</div>
+						<div className="AbstractPicture-layer2-obj11_5">
+							<span></span>
+							<span></span>
+						</div>
+						<div className="AbstractPicture-layer2-obj12"></div>
+						<div className="AbstractPicture-layer2-obj13"></div>
+						<div className="AbstractPicture-layer2-obj14">
+							<span></span>
+						</div>
+						<div className="AbstractPicture-layer2-obj15">
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+						<div className="AbstractPicture-layer2-obj16">
+							<span></span>
+						</div>
+						<div className="AbstractPicture-layer2-obj17"></div>
+						<div className="AbstractPicture-layer2-obj18"></div>
+						<div className="AbstractPicture-layer2-obj19"></div>
+						<div className="AbstractPicture-layer2-obj20"></div>
+						<div className="AbstractPicture-layer2-obj21"></div>
+						<div className="AbstractPicture-layer2-obj22"></div>
+						<div className="AbstractPicture-layer2-obj23"></div>
+						<div className="AbstractPicture-layer2-obj24"></div>
+						<div className="AbstractPicture-layer2-obj25"></div>
+						<div className="AbstractPicture-layer2-obj26"></div>
+						<div className="AbstractPicture-layer2-obj27"></div>
+						<div className="AbstractPicture-layer2-obj28"></div>
+						<div className="AbstractPicture-layer2-obj29"></div>
+						<div className="AbstractPicture-layer2-obj30"></div>
+						<div className="AbstractPicture-layer2-obj31"></div>
+						<div className="AbstractPicture-layer2-obj32"></div>
+						<div className="AbstractPicture-layer2-obj33"></div>
+						<div className="AbstractPicture-layer2-obj34"></div>
+						<div className="AbstractPicture-layer2-obj35"></div>
+						<div className="AbstractPicture-layer2-obj36"></div>
+					</div>
+					<div className="AbstractPicture-layer1">
+						<div className="AbstractPicture-layer1-obj1"></div>
+						<div className="AbstractPicture-layer1-obj2"></div>
+						<div className="AbstractPicture-layer1-obj3"></div>
+						<div className="AbstractPicture-layer1-obj4"></div>
+						<div className="AbstractPicture-layer1-obj5"></div>
+						<div className="AbstractPicture-layer1-obj6"></div>
+						<div className="AbstractPicture-layer1-obj7"></div>
+						<div className="AbstractPicture-layer1-obj8"></div>
+					</div>
+				</div>
+			</HomeHeaderStyle>
+		</>
+	);
+}
